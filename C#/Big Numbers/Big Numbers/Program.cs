d6441c80092ed14d616b7624cfd0c8f3fe653877
Baker22 <alex811022@ya.ru>
2016-05-05 14:04:43 +0300

    big numbs

---
+++ C#/Big Numbers/Big Numbers/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Big_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Big Numbers";
+            Console.Write("Введите число : ");
+            
+            string[] one = { "   1   ", "  11   ", " 1 1   ", "   1   ", "   1   ", "   1   ", "   1   ", "  111  " };
+            string[] two = { "  222  ", " 2   2 ", " 2   2 ", "     2 ", "    2  ", "   2   ", "  2    ", " 22222 " };
+            string[] three = { "  333  ", " 3   3 ", "     3 ", "    33 ", "     3 ", "     3 ", " 3   3 ", "  333  " };
+            string[] four = { " 4   4 ", " 4   4 ", " 4   4 ", "  4444 ", "     4 ", "     4 ", "     4 ", "     4 " };
+            string[] five = { " 55555 ", " 5     ", " 5     ", " 5555  ", "     5 ", "     5 ", "     5 ", " 5555  " };
+            string[] six = { "  666  ", " 6   6 ", " 6     ", " 6666  ", " 6   6 ", " 6   6 ", " 6   6 ", "  666  " };
+            string[] seven = { " 77777 ", "     7 ", "    7  ", "   7   ", "   7   ", "   7   ", "   7   ", "   7   " };
+            string[] eight = { "  888  ", " 8   8 ", " 8   8 ", "  888  ", " 8   8 ", " 8   8 ", " 8   8 ", "  888  " };
+            string[] nine  = { "  999  ", " 9   9 ", " 9   9 ", "  9999 ", "     9 ", "     9 ", " 9   9 ", "  999  " };
+            string[] zero = { "  000  ", " 0   0 ", " 0   0 ", " 0   0 ", " 0   0 ", " 0   0 ", " 0   0 ", "  000  " };
+            string num = Console.ReadLine();
+            int quant = num.Count();
+            Console.WriteLine();
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < quant; j++)
+                {
+                    switch (num.ElementAt(j))
+                    {
+                        case '1':
+                            Console.Write(one[i]);
+                            break;
+                        case '2':
+                            Console.Write(two[i]);
+                            break;
+                        case '3':
+                            Console.Write(three[i]);
+                            break;
+                        case '4':
+                            Console.Write(four[i]);
+                            break;
+                        case '5':
+                            Console.Write(five[i]);
+                            break;
+                        case '6':
+                            Console.Write(six[i]);
+                            break;
+                        case '7':
+                            Console.Write(seven[i]);
+                            break;
+                        case '8':
+                            Console.Write(eight[i]);
+                            break;
+                        case '9':
+                            Console.Write(nine[i]);
+                            break;
+                        case '0':
+                            Console.Write(zero[i]);
+                            break;
+                        default:
+                            break;
+ 
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+         
+    }
+}
--- C#/Stairs/ConsoleApplication1/Program.cs
+++ C#/Stairs/ConsoleApplication1/Program.cs
@@ -1,35 +1,26 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 
 namespace ConsoleApplication1
 {
+
     class Program
     {
-        static int stairs = Convert.ToInt32(Console.ReadLine());
-        static int spaceStair = 0;
-        static int downSpace =2;
         static void Main(string[] args)
         {
-            for (int i = 0; i < stairs;i++ )
+            while (true)
             {
-                for (int j = 0; j < spaceStair; j++)
-                    Console.Write(' ');
-                Console.WriteLine("***");
-                 for (int t = 0; t < downSpace; t++)
-                     Console.Write(' ');
-                Console.WriteLine("*");
-                    spaceStair += 2;
-                    downSpace += 2;
-                if(i==stairs-1)
+                if (Console.KeyAvailable == true)
                 {
-                    for (int t = 0; t < downSpace-2; t++)
-                        Console.Write(' ');
-                    Console.WriteLine("***");
+                    ConsoleKeyInfo k = Console.ReadKey(true);
+                    Console.Write(k.KeyChar);
                 }
-            }       
+                else
+                {
+                    Console.Write("_");
+                }
+                Thread.Sleep(15);
+            }
         }
     }
 }

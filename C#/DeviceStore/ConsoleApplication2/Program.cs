c0e1da5f551d18ad0ab2e481b3e4d038f2222231
Baker22 <alex811022@ya.ru>
2016-06-11 14:49:35 +0300

    1

---
+++ C#/DeviceStore/ConsoleApplication2/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+abstract class Device 
+{
+    private int price;
+    private int warranty;
+    private string category;
+    private string producer;
+    private string dateOfIssue;
+    private string model;
+
+     public Device(int price,int warranty,string category, string producer, string dateOfIssue, string model) 
+    {
+        this.price = price;
+        this.warranty=warranty;
+        this.category=category;
+        this.producer = producer;
+        this.dateOfIssue=dateOfIssue;
+        this.model=model;
+    }
+    public int Price
+    {
+        get {return price;}
+        set
+        {
+            if (value > 0) price = value;
+        }
+    }
+    public int Warranty
+    {
+        get {return warranty;}
+        set
+        {
+            if (value > 0) warranty = value;
+        }
+    }
+    public string Producer
+    {
+        get {return producer;}
+        set
+        {
+            if(value!=null)
+            producer = value;
+        }
+    }
+    public string Category
+    {
+        get {return category;}
+        set
+        {
+            if (value != null)
+                category = value;
+        }
+    }
+    public string DateOfIssue
+    {
+        get { return dateOfIssue; }
+        set
+        {
+            if (value != null)
+                dateOfIssue = value;
+        }
+    }
+    public string Model
+    {
+        get { return model; }
+        set
+        {
+            if (value != null)
+                model = value;
+        }
+    }
+    public override String ToString()
+{
+    return "Price: " + price + "\n warranty: $" + warranty+"\n Category: "+category+"\n Producer: "+producer+"\n Date of issue: "+dateOfIssue+"\n Model: "+model;
+}
+}
+
+
+class Laptop : Device
+{
+    public Laptop(int price, int warranty, string category, string producer, string dateOfIssue, string model) : base(price,warranty,category, producer, dateOfIssue, model) { }
+}
+class Tablet : Device
+{
+    public Tablet(int price, int warranty, string category, string producer, string dateOfIssue, string model) : base(price, warranty, category, producer, dateOfIssue, model) { }
+}
+class Phone : Device
+{
+    public Phone(int price, int warranty, string category, string producer, string dateOfIssue, string model) : base(price, warranty, category, producer, dateOfIssue, model) { }
+}
+class Charger : Device
+{
+    public Charger(int price, int warranty, string category, string producer, string dateOfIssue, string model) : base(price, warranty, category, producer, dateOfIssue, model) { }
+}
+class Case : Device
+{
+    public Case(int price, int warranty, string category, string producer, string dateOfIssue, string model) : base(price, warranty, category, producer, dateOfIssue, model) { }
+}
+
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////
+
+class Store
+{
+    private Device[] devices;
+
+    uint devicesSize = 0;
+   
+public Store(int sizeOfStore)
+{
+    devices = new Device[sizeOfStore];
+ 
+}
+    public void AddDevice(Device _new)
+{
+    devices[devicesSize] = _new;
+    devicesSize++;
+}
+  public void SearchByPrice(int min,int max)
+{
+    for (int i = 0; i < devicesSize;i++ )
+    {
+        if (devices[i].Price > min && devices[i].Price < max)
+            Console.WriteLine(devices[i].ToString());
+    }
+}
+    public void SearchByModel(string mod)
+  {
+      Regex reg = new Regex(mod);
+        for(int i=0;i<devicesSize;i++)
+        {
+            Match mat = reg.Match(devices[i].Model);
+                if(mat.Success)
+                Console.WriteLine(devices[i].ToString());
+        }
+  }
+
+public Device this[uint index]
+{
+    get
+    {
+        if (index >= devices.Length)
+        {
+            throw new IndexOutOfRangeException();
+        }
+        else
+        {
+            return devices[index];
+        }
+    }
+    set
+    {
+        devices[index] = value as Device;
+    }
+}
+public Device this[string date]
+{
+    get
+    {
+        for (int i = 0; i < devicesSize; i++)
+        {
+            if (devices[i].DateOfIssue == date)
+                return devices[i];  
+        }
+            return null;
+    }
+}
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Store s = new Store(30);
+        Device a = new Laptop(1500, 2, "Laptops", "Toshiba", "2016", "The best");//(price,warranty,category, producer, dateOfIssue, model)
+        Device b = new Tablet(1000, 1, "Tablets", "Samsung", "2016", "The thinkest");
+        s.AddDevice(a);
+        s.SearchByPrice(1000, 1600);
+        s.SearchByModel("The best");
+        Console.WriteLine(s["2016"]);
+        /*s[0] = new Laptop("Samsung", 5200);
+        s[1] = new Laptop("Asus", 4700);
+        s[2] = new Laptop("Toshiba", 4300);
+
+        try
+        {
+            for (uint i = 0; i < s.Length; i++)
+            {
+                Console.WriteLine(s[i]);
+            }
+        }
+        catch (NullReferenceException exception)
+        {
+            Console.WriteLine(exception.Message);
+        }
+    }*/
+    }
+}
---
+++ C#/LoginPassCillection/LoginPassCillection/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoginPassCollection
+{
+    public class Pair
+    {
+        private string login;
+        private string password;
+        public Pair(string login,string password)
+        {
+            SetLog(login);
+            SetPass(password);
+        }
+        private void SetLog(string login)
+        {
+            this.login=login;
+        }
+        private void SetPass(string password)
+        {
+            this.password = password;
+        }
+        public string Login
+        {
+            get{return login;}
+            set
+            {
+                login = value;
+            }
+        }
+        public string Password 
+        { 
+            get { return password; }
+            set
+            {
+                password = value;
+            }
+        }
+    }
+       public class Collection
+    {
+           ShowMenu x=new ShowMenu();
+            Pair[] arr;
+            uint count=0;
+            public Collection(uint size)
+            {
+                arr = new Pair[size];
+                x.Show();
+            }
+            public int this[string log]
+        {
+            get
+            {
+                for(int i=0;i<count;i++)
+                {
+                    if (arr[i].Login == log)
+                        return i;
+                }
+                throw new Exception("no such login");
+            }
+        }
+            
+            public override string ToString()
+            {
+                string res="";
+                for (int i = 0; i <count; i++)
+                {
+                    res = res + "Login: " + arr[i].Login + ".\n" + "Password: " + arr[i].Password + ".\n\n";
+                }
+                return res;
+            }
+           public void ShowAll(){Console.WriteLine(this.ToString());} 
+           public void AddNewUser(Pair _new)
+            {
+                arr[count] = _new;
+                count++;
+            }
+           public void DeleteUser(string log)
+           {
+               Pair[] temp = new Pair[arr.Length];
+               for (int i = 0; i < this[log]; i++)
+                       temp[i] = arr[i];
+               for (int j = this[log]; j < count-1; j++)
+                   temp[j] = arr[j + 1];
+               arr = temp;
+               count--;
+           }
+           public void Edit(string log)
+           {
+               int index=this[log];
+               Console.WriteLine("Enter a new Login");
+               arr[index].Login = (Console.ReadLine());
+               Console.WriteLine("Enter a new Password");
+               arr[index].Password = (Console.ReadLine());
+               Console.WriteLine("\n");
+           }
+           public class ShowMenu
+           {
+               public void Show()
+               {
+                   Console.WriteLine("      MENU\n ");
+               Console.WriteLine(" For open all collection Enter \"ShowAll()\"");
+               Console.WriteLine(" For add new user Enter \"AddNewUser(Pair _new)\"");
+               Console.WriteLine(" For delete user by login Enter \"DeleteUser(string log)\"");
+               Console.WriteLine(" For edit user by login Enter \"Edit(string log)\"");
+
+               }
+
+           }
+        }
+  
+    
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Collection col = new Collection(30);
+            Pair a = new Pair("Alex","Baker22");
+            Pair b=new Pair("timur","batrutdinov");
+            Pair c=new Pair("sergey","2222");
+            Pair d=new Pair("dima","ursu");
+            col.AddNewUser(a);
+            col.AddNewUser(b);
+            col.AddNewUser(c);
+            col.AddNewUser(d);
+            col.ShowAll();
+            col.DeleteUser("timur");
+            col.ShowAll();
+            col.Edit("sergey");
+            col.ShowAll();
+        }
+    }
+}
